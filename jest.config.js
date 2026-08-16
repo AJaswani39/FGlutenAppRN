76a0f187a05d61d43090ea4c0715f6048a9861d2
AJaswani39 <jaswanianagha8@gmail.com>
2026-08-16 11:23:16 -0400

    Update jest.config.js to ignore cloud-run-proxy directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/android/'],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/cloud-run-proxy/'],
 };
